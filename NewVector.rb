30c8c11d53397f3fd565566aa7fd1738ce9e39df
d1240 <d1240>
2016-09-28 12:58:01 +0900

    変更後

--- NewVector.rb
+++ NewVector.rb
@@ -2,21 +2,37 @@ require 'complex'
 
 class Vector
   def initialize(x,y)
-	@x = x
-	@y = y
+    @x = x
+    @y = y
   end
 
   def to_s
-       return "(#{@x},#{@y})"
+    return "(#{@x},#{@y})"
   end
   
-  def length
-	@X = @x * @x
-	@Y = @y * @y
-	@L = @X + @Y
-        @l =  Math.sqrt(@L)
-	return @l
+  def X
+    @x
   end
+
+  def Y
+    @y
+  end
+
+  def length
+    @X = @x * @x
+    @Y = @y * @y
+    @L = @X + @Y
+    @l =  Math.sqrt(@L)
+    return @l
+  end
+  
+  def add(v)
+    newX = @x + v.X
+    newY = @y + v.Y
+    new = Vector.new(newX,newY)
+    return new
+  end
+
 end
 
 
